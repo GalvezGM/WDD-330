08e19f237c8fa207d7b23e1b93ae737a669b2956
GalvezGM <63634928+GalvezGM@users.noreply.github.com>
2020-09-20 11:16:03 +0800

    Week 1 Changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-const links = [
-    {
-      label: "Week1 notes",
-      url: "week1/index.html"
+function tableOfContents() {
+    const links = [
+        {
+          label: "Week1 notes",
+          url: "week1/index.html"
+        }
+      ];
+    var ol = document.createElement("ol");
+    var li, a, text;
+    for (var i = 0;  i < links.length; ++i) {
+      li = document.createElement('li');
+      a  = document.createElement('a');
+      text = document.createTextNode(links[i]["label"]);
+  
+      a.href = links[i]["url"];
+      a.appendChild(text);
+      li.appendChild(a);
+      ol.appendChild(li);
+  
     }
-  ]
+    document.querySelector("#tableOfContents").appendChild(ol);
+  }
+  tableOfContents();
